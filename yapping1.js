fe0b48d919b60c1e8d9ba6e4f013c005f3d511b1
mystrcrious <stephanieaurelia07@gmail.com>
2024-12-31 00:07:33 +0700

    Add files via upload

---
+++ yapping1.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('audioplayer');
+    const savedtime = localStorage.getItem('audiocurrenttime');
+
+    if (savedtime !== null) {
+        audio.Currenttime = savedtime;
+    }
+
+    audio.addEventListener('timeupdate', function() {
+        localStorage.setItem('audiocurrenttime', audio.currenttime);
+    });
+});
+
+function navigate() {
+    window.location.href = 'yapping2.html';
+}
---
+++ yapping2.js
@@ -0,0 +1,3 @@
+function navigate() {
+    window.location.href = 'yapping3.html';
+}
